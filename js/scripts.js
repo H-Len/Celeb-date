f3223b9da044c3254bbff76409bac3b42ad76880
hannah melendy, Jake Nagel and Nathan Aden <jake@>
2019-03-28 13:36:40 -0700

    return values from check boxes and buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
   // $('#gate-keeper').modal({backdrop: 'static', keyboard: false});
-  $("#age-submit").click(function(){
-    var ageChecker = $('#age-varification').is(":checked");
+  $("#user-data").submit(function(event){
+    event.preventDefault();
+    var hats = $('#form-hats').is(":checked");
+    var poetInput = $("#form-poet").is(":checked");
+    var litpref = $('#form-cats input:radio:checked').val();
 
-    if(ageChecker === true) {
-      $("#gate-keeper").modal('hide');
-    } else {
-      $("#gate-keeper").effect("shake");
-    }
 
+    console.log("The cats box is " + litpref);
+    // var ageChecker = $('#age-varification').is(":checked");
+    //
+    // if(ageChecker === true) {
+    //   $("#gate-keeper").modal('hide');
+    // } else {
+    //   $("#gate-keeper").effect("shake");
+    // }
 
 
 
